6bb86f9f548c2fefdc82d567cd33132a57281e3a
D0r14nb <81080272+D0r14nb@users.noreply.github.com>
2025-05-14 00:49:01 +0200

    another fix attempt

--- wall_door_interactible.gd
+++ wall_door_interactible.gd
@@ -2,7 +2,8 @@ extends Node3D
 
 enum DoorType{
 	MANUAL,
-	LEVER,
+	OPEN,
+	CLOSED,
 }
 
 @export var door_type : DoorType = DoorType.MANUAL
@@ -17,22 +18,28 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if door_type == DoorType.MANUAL:
-		get_node("wallDoor/door").set("rotation_degrees", Vector3(0, -min(max(-atan((get_node("wallDoor/door").position.z - get_node("wallDoor/GrabPoint").position.z)/(get_node("wallDoor/door").position.x - get_node("wallDoor/GrabPoint").position.x)) * 180 / PI, -160),160), 0))
+		get_node("wallDoor/door").set("rotation_degrees", Vector3(0, min(max(atan((get_node("wallDoor/door").position.z - get_node("wallDoor/GrabPoint").position.z)/(get_node("wallDoor/door").position.x - get_node("wallDoor/GrabPoint").position.x)) * 180 / PI, -160),160), 0))
 		
 		if get_node("wallDoor/door").position.distance_to(get_node("wallDoor/GrabPoint").position) > 2: # Checks if the grabPoint is in an awkward position
 			get_node("wallDoor/GrabPoint").drop() # Force release the lever from the hand
-
-# Opens an automated door
-func open() -> void:
-	if door_type == DoorType.LEVER:
-		while(get_node("wallDoor/door").rotation_degrees.y < 120):
-			get_node("wallDoor/door").set("rotation_degrees.y", get_node("wallDoor/door").rotation_degrees.y + 3)
+	
+	if door_type == DoorType.OPEN and get_node("wallDoor/door").rotation_degrees.y < 120:
+		get_node("wallDoor/door").set("rotation_degrees.y", get_node("wallDoor/door").rotation_degrees.y + 3)
 
 # Closes an automated door
+	if door_type == DoorType.CLOSED and get_node("wallDoor/door").rotation_degrees.y > 0:
+		get_node("wallDoor/door").set("rotation_degrees.y", get_node("wallDoor/door").rotation_degrees.y - 3)
+
+# Sets an automated door to open
+func open() -> void:
+	if door_type == DoorType.CLOSED:
+		door_type = DoorType.OPEN
+
+# Sets an automated door to close
 func close() -> void:
-	if door_type == DoorType.LEVER:
-		while(get_node("wallDoor/door").rotation_degrees.y > 0):
-			get_node("wallDoor/door").set("rotation_degrees.y", get_node("wallDoor/door").rotation_degrees.y - 3)
+	if door_type == DoorType.OPEN:
+		door_type = DoorType.CLOSED
+
 
 func snapDoor() -> void:
 	var curPos = get_node("wallDoor/GrabPoint").position
