c5af33b77cd12ad70c31f3bed93564410fddbc20
D0r14nb <81080272+D0r14nb@users.noreply.github.com>
2025-05-14 00:20:47 +0200

    new fix

--- wall_door_interactible.gd
+++ wall_door_interactible.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if door_type == DoorType.MANUAL:
-		get_node("wallDoor/door").set("rotation_degrees", Vector3(0, min(max(-atan((get_node("wallDoor/door").position.x - get_node("wallDoor/GrabPoint").position.x)/(get_node("wallDoor/door").position.z - get_node("wallDoor/GrabPoint").position.z)) * 180 / PI, -45),45), 0))
+		get_node("wallDoor/door").set("rotation_degrees", Vector3(0, -min(max(-atan((get_node("wallDoor/door").position.z - get_node("wallDoor/GrabPoint").position.z)/(get_node("wallDoor/door").position.x - get_node("wallDoor/GrabPoint").position.x)) * 180 / PI, -160),160), 0))
 		
 		if get_node("wallDoor/door").position.distance_to(get_node("wallDoor/GrabPoint").position) > 2: # Checks if the grabPoint is in an awkward position
 			get_node("wallDoor/GrabPoint").drop() # Force release the lever from the hand
@@ -33,3 +33,10 @@ func close() -> void:
 	if door_type == DoorType.LEVER:
 		while(get_node("wallDoor/door").rotation_degrees.y > 0):
 			get_node("wallDoor/door").set("rotation_degrees.y", get_node("wallDoor/door").rotation_degrees.y - 3)
+
+func snapDoor() -> void:
+	var curPos = get_node("wallDoor/GrabPoint").position
+	get_node("GrabPoint").set("position", curPos * 0.4 / curPos.distance_to(Vector3(0,0,0)))
+
+func _on_grab_point_dropped(pickable: Variant) -> void:
+	snapDoor()
